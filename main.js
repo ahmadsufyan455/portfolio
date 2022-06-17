8a65cf9fbf6454b0911b93199f6f50454e371651
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-06-17 22:10:31 +0700

    animate section when scrolling down

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 function downloadCv() {
    alert('On progress...')
 }
+
+$(function(){  // $(document).ready shorthand
+  $('.header').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.fade-in').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
